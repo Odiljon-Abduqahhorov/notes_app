5f79e5d9bb61477d6231a11e8f62225040c359e7
Odiljon Abduqaxxarov <88959988+Odiljon-Abduqahhorov@users.noreply.github.com>
2023-12-21 16:40:05 +0500

    Update modules.py

--- TODO App/modules.py
+++ TODO App/modules.py
@@ -1,14 +1,94 @@
 import random
+import json
 
 def showNotes(notes):
     print("Your notes:")
-    for i in notes.keys():
-        print(f"\t\t> {notes[i]['name']} : {notes[i]['text']}")
-        print(f"\t\t{' ' * 20}ID:  {i}")
-    if not notes:
-        print(f"\t\tThere is no notes")
-    print('\n')
 
+    for i in notes.keys():
+        name = notes[i]['name']
+        text = f"| {notes[i]['text']}"
+        textInArray = list(text)
+        idd = f"ID: {i}"
+
+        lent = len(text)
+        c = 1
+
+        k = 100
+        while lent >= 100:
+            textInArray[c*k] += " |\n\t\t| "
+
+            lent -= 100
+            c += 1
+        
+        text = ""
+        for z in textInArray:
+            text += z
+
+        text += (101 - lent) * " " + "|"
+
+        print(
+            f'''
+                 ---- {name} {'-' * (100 - len(name) - 5)}
+                {text}
+                 {'-' * (100 - len(idd) - 5)} {idd} ----
+            '''
+        )
+
+    if not notes:
+        print(
+            '''
+                 -------------------
+                | There is no notes |
+                 -------------------
+            '''
+        )
+
+def nameAndTextChecker(name, text, notes, idd, Note):
+    if (name == "" or name.isspace() or len(name) > 50) or (text == "" or text.isspace()):
+        if (name == "" or name.isspace() or len(name) > 50) and (text == "" or text.isspace()):
+            print("Invalid Name and Text")
+        elif name == "" or name.isspace() or len(name) > 50:
+            print("Invalid Name")
+        else:
+            print("Invalid Text")
+    else:
+        if Note == None:
+            notes[idd]['name'] = name
+            notes[idd]['text'] = text
+
+            with open('notes.json', 'w') as file:
+                json.dump(notes, file)
+
+            print("Done :)")
+        else:
+            note = Note(name, text)
+            notes[f'{note.generateId()}'] = {"name": note.getName(), "text": note.getText()}
+            with open('notes.json', 'w') as file:
+                json.dump(notes, file)
+
+            print("Done :)")
+
+def nameChecker(newName, notes, idd):
+    if newName == "" or newName.isspace() or len(newName) > 50:
+        print("Invalid Name")
+    else:
+        notes[idd]['name'] = newName
+
+        with open('notes.json', 'w') as file:
+            json.dump(notes, file)
+
+        print("Done :)")
+
+def textChecker(newText, notes, idd):
+    if newText == "" or newText.isspace():
+        print("Invalid Text")
+    else:
+        notes[idd]['text'] = newText
+
+        with open('notes.json', 'w') as file:
+            json.dump(notes, file)
+
+        print("Done :)")
 
 class Note:
     def __init__(self, nname, ntext):
